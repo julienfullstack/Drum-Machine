7380c0dcf8044f1937faeca0f57dcf38abd90f2a
Julien L <julienlenaz96@gmail.com>
2024-02-13 17:49:36 -0500

    Added 303 sawtooth sounds

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,19 @@ function App() {
   return (
     <DrumMachine
       samples={[
-        { url: "/hat-closed.wav", name: "CH" }
+        { url: "/B.wav", name: "B" },
+        { url: "/Asharp.wav", name: "A#" },
+        { url: "/A.wav", name: "A" },
+        { url: "/Gsharp.wav", name: "G#" },
+        { url: "/G.wav", name: "G" },
+        { url: "/Fsharp.wav", name: "F#" },
+        { url: "/F.wav", name: "F" },
+        { url: "/E.wav", name: "E" },
+        { url: "/Dsharp.wav", name: "D#" },
+        { url: "/D.wav", name: "D" },
+        { url: "/Csharp.wav", name: "C#" },
+        { url: "/C.wav", name: "C" },
+
       ]}
     />
   );
